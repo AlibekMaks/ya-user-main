96b0bdcb3a465989541f936491f63670f5b4af20
Alibek <a.maksut@platonus.kz>
2024-03-25 16:35:53 +0500

    user first commit

---
+++ src/test/java/com/ya/yausermain/YaUserMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ya.yausermain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaUserMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
